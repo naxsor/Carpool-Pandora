913fc645aeefdff4fdf4de6d14b279f2b4fad75d
naxsor <lemanuel.colon@upr.edu>
2023-05-11 18:21:39 -0400

    -Changed app for already dev one -Added content component -added layout component -added nav component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Layout from './components/Layout.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    data: null
+  };
+  children = {
+    text_1:"Demo 1",
+    text_2:"Demo 2"
+  };
+
+  componentDidMount() {
+    this.callBackendAPI()
+        .then(res => this.setState({ data: res.express }))
+        .catch(err => console.log(err));
+  }
+  // fetching the GET route from the Express server which matches the GET route from server.js
+  callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message)
+    }
+    return body;
+  };
+
+  render() {
+    return (
+        <div className="App">
+          <Layout>
+            {this.children}
+          </Layout>
+          <p className="App-intro">{this.state.data}</p>
+        </div>
+    );
+  }
 }
 
 export default App;
